2865d29c246b45b4d04a8464864280b000f29a22
Ammar Al-Haddad <alhaddadam@gmail.com>
2021-12-29 13:19:42 -0500

    final push

---
+++ src/dataBase.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 1,
+    name: "Mero Haddad",
+    age: 38,
+    image:
+      "https://res.cloudinary.com/dyz7iuvrr/image/upload/v1640734450/jewish_nobsfl.png",
+  },
+  {
+    id: 2,
+    name: "peter san",
+    age: 33,
+    image:
+      "https://res.cloudinary.com/dyz7iuvrr/image/upload/v1640734450/jewish_nobsfl.png",
+  },
+  {
+    id: 3,
+    name: "Chad Koury",
+    age: 38,
+    image:
+      "https://res.cloudinary.com/dyz7iuvrr/image/upload/v1640734450/jewish_nobsfl.png",
+  },
+  {
+    id: 4,
+    name: "sam Gillis",
+    age: 40,
+    image:
+      "https://res.cloudinary.com/dyz7iuvrr/image/upload/v1640734450/jewish_nobsfl.png",
+  },
+];
